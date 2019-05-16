12be8bed47ed10dc39c2cfb26d730f0bdadf9982
ayman-93 <aymanfaisal.cs@gmail.com>
2019-05-17 02:01:12 +0300

    change the port to process.env.port

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import signin from "./controllers/Signin";
 import { imageHandle, apiCall } from "./controllers/Image";
 import getProfile from "./controllers/Profile";
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port;
 const db = knex({
   client: "pg",
   connection: {
